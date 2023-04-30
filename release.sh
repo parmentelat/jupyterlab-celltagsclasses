f141ce279173ba22b11ae770d1ee45083fa8f126
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-04-30 15:41:20 +0200

    release.sh is a helper shel command to publish a new version

---
+++ release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Current version is: $(hatch version)"
+
+# prompt user for version number
+echo "Enter new version number: "
+read version
+
+echo "checking for required python packages"
+for pkg in build twine hatch; do
+    pip show $pkg >& /dev/null || pip install $pkg
+done
+
+# hatch has the nasty effect of reformatting package.json
+#hatch version $version
+echo "bumping version to $version"
+sed -e 's/\("version": "\)[^"]*/\1'$version'/' package.json > package.json.tmp
+mv package.json.tmp package.json
+
+echo "diffing package.json"
+git diff package.json
+echo "want to go on ? (ctrl-c to abort)"
+read answer
+
+echo "cleaning dist/, and rebuilding"
+rm -rf dist/*
+python -m build
+echo "publishing to pypi"
+python -m twine upload dist/*
+
+# commit and tag
+echo "committing and tagging"
+git add package.json
+git commit -m "release $version"
+git tag "v$version"
